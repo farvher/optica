e1c0a20e04e5cf5e6197f31887f22e9a3152fe81
farvher <fesanmiguel@misena.edu.co>
2017-11-08 16:21:25 -0500

    first commit

---
+++ src/main/java/com/optica/OpticaApplication.java
@@ -0,0 +1,12 @@
+package com.optica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticaApplication.class, args);
+	}
+}
