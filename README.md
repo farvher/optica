19ae4bafea91fc305108378f750b6bbce26f0951
Farith Sanmiguel <farith.sanmiguel@middlesoftware.com>
2018-08-27 11:38:09 -0500

    cambio de planes

---
+++ README.md
@@ -0,0 +1,11 @@
+#Portal de empleo para desarrolladores 
+
+### FEATURES
+
+** OFERTAS Y APLICACION **
+ 
+** CREACION DE USUARIOS RAPIDO**
+
+** ADMINISTRACION DE APLICACION**
+
+** **
--- src/main/java/com/optica/OpticaApplication.java
+++ src/main/java/com/optica/OpticaApplication.java
@@ -10,15 +10,6 @@ public class OpticaApplication {
 
 	/**
 	 *
-	 * el usuario se registra e importa contactos de su cel
-     * enviara un mensaje de texto a n contactos
-     * enviara un mensaje de texto personalizado basado  en columnas
-     * enviara mensaje a travez de un web service
-     * limita la cantidad de mensajes por membresia
-     * informacion de membresia comprada
-	 * el usuario se registra pero no se activara de inmediato solo enviara 3 msj
-	 * el administrador habilita al usuario
-	 *
 	 *
      *
 	 * */
